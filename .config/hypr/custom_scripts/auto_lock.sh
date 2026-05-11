acce7afb85c3578e2aa8f3a3eaa71e0c61a47c07
shinkuan <shinkuan318@gmail.com>
2026-05-11 23:42:00 +0800

    feat: add mimeapps.list for application associations

--- .config/hypr/custom_scripts/auto_lock.sh
+++
@@ -1,4 +0,0 @@
-while ! qs -c caelestia log | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,3})*)?[mGK]//g" | grep " INFO: Configuration Loaded"; do
-  sleep 0.01
-done
-hyprctl dispatch global caelestia:lock
