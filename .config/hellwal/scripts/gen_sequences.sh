0d63c211bfb8b53d9cf44c99104ab215b66906a9
shinkuan <shinkuan318@gmail.com>
2026-05-12 09:38:09 +0800

    fix: hyprctl reload after gen_sequences.sh

--- .config/hellwal/scripts/gen_sequences.sh
+++ .config/hellwal/scripts/gen_sequences.sh
@@ -4,3 +4,5 @@ shift
 hellwal -i "$WALLPAPER_PATH" --check-contrast "$@"
 # mkdir -p "$HOME/.local/state/caelestia"
 # cp "$HOME/.cache/hellwal/sequences.txt" "$HOME/.local/state/caelestia/sequences.txt"
+
+hyprctl reload
