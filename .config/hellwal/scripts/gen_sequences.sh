80adf63c4d410aae846e1496cef73b01259ac960
shinkuan <shinkuan318@gmail.com>
2026-06-24 11:10:28 +0800

    tweak hellwal hook script

--- .config/hellwal/scripts/gen_sequences.sh
+++ .config/hellwal/scripts/gen_sequences.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-shift
+echo $WALLPAPER_PATH
 hellwal -i "$WALLPAPER_PATH" --check-contrast "$@"
 # mkdir -p "$HOME/.local/state/caelestia"
 # cp "$HOME/.cache/hellwal/sequences.txt" "$HOME/.local/state/caelestia/sequences.txt"
